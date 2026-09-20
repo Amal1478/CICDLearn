1ef9d030a1a6e0e57953c6938e7ee964b0fdab88
Amal <amaldev147@gmail.com>
2026-09-20 14:36:44 +0530

    test run on code change

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -107,5 +107,5 @@ test.describe('OrangeHRM Login Feature', () => {
         // The URL should change to request password reset code page
         await expect(page).toHaveURL(/.*requestPasswordResetCode/);
     });
-    //New change
+    //New updated change.
 });
